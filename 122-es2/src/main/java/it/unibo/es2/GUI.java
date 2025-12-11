95c34e1392188f5d4cf8d5ff0db66d0e77c33568
Pietro Dapporto <dapportopietro@gmail.com>
2025-12-11 17:14:39 +0100

    done es 122

--- 122-es2/src/main/java/it/unibo/es2/GUI.java
+++ 122-es2/src/main/java/it/unibo/es2/GUI.java
@@ -17,7 +17,7 @@ public final class GUI extends JFrame {
     @Serial
     private static final long serialVersionUID = 1L;
     private final Map<JButton, Pair<Integer, Integer>> buttons = new LinkedHashMap<>();
-    //private final Logics logics;
+    private final transient Logics logics;
 
     /**
      * Constructs a GUI with the specified size.
@@ -25,7 +25,7 @@ public final class GUI extends JFrame {
      * @param size the size of the grid
      */
     public GUI(final int size) {
-        //this.logics = new LogicsImpl(size);
+        this.logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100 * size, 100 * size);
         // Layout
@@ -36,9 +36,8 @@ public final class GUI extends JFrame {
             for (int j = 0; j < size; j++) {
                 final JButton button = new JButton(" ");
                 button.addActionListener(e -> {
-                    final Pair<Integer, Integer> buttonPosition = buttons.get(button);
-                    button.setText(buttonPosition.toString());
-                    if (buttonPosition.equals(new Pair<>(0, 0))) {
+                    button.setText(logics.hit(buttons.get(button)));
+                    if (logics.toQuit()) {
                         // System.exit(1); // Too brutal!
                         dispose();
                     }
